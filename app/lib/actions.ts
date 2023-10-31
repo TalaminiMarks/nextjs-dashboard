e8caf0a97ac419135eb2b5c7d320834edc2df629
TalaminiMarks <talamini2014@gmail.com>
2023-10-31 13:12:28 -0300

    coisas2

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import z from 'zod'
 import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
 const InvoiceSchema = z.object({
   id: z.string(),
@@ -11,7 +13,9 @@ const InvoiceSchema = z.object({
   date: z.string()
 })
 
-const CreateInvoice = InvoiceSchema.omit({id: true, date: true})
+const CreateInvoice = InvoiceSchema.omit({id: true, date: true});
+
+const UpdateInvoice = InvoiceSchema.omit({ date: true, id: true });
 
 export async function createInvoice(formData: FormData){
   const {amount, status, customerId} = CreateInvoice.parse({
@@ -26,4 +30,32 @@ export async function createInvoice(formData: FormData){
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+  
+export async function updateInvoice(id: string, formData: FormData) {
+  const {amount, status, customerId} = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+ 
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
